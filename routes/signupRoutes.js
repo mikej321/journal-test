40d70f77196d77247f47e404f112c735fdf654bf
Michael Johnson <michaeljcantley@gmail.com>
2024-09-01 11:02:44 -0400

    Third Commit

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,26 +1,49 @@
 const Router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const prisma = require("../prisma");
+const { body, validationResult } = require("express-validator");
 
 Router.get("/", (req, res, next) => {
   res.render("signup");
 });
 
-Router.post("/", async (req, res, next) => {
-  const { username, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
-    try {
-      await prisma.user.create({
-        data: {
-          username,
-          password: hashedPassword,
-        },
+Router.post(
+  "/",
+  // using validation to verify valid inputs (middleware)
+  [
+    body("username").notEmpty().withMessage("Cannot be empty").trim(),
+    body("password")
+      .notEmpty()
+      .withMessage("Cannot be empty")
+      .isStrongPassword()
+      .withMessage("Password not strong enough")
+      .trim(),
+  ], // Be sure to not have an extra comma or it won't work
+  async (req, res, next) => {
+    // start of the validationResult middleware
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
       });
-      res.redirect("/");
-    } catch (err) {
-      return next(err);
     }
-  });
-});
+
+    const { username, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      try {
+        await prisma.user.create({
+          data: {
+            username,
+            password: hashedPassword,
+          },
+        });
+        res.redirect("/");
+      } catch (err) {
+        return next(err);
+      }
+    });
+  }
+);
 
 module.exports = Router;
